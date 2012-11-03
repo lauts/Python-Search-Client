31b4c330c4b896270668e2c1e1bd2fd4167b2d17
Lauts <alauter@gmail.com>
2012-11-02 21:53:36 -0400

    Search Twitter from the command line (pt 1)

--- twitter.py
+++ twitter.py
@@ -1,3 +1,22 @@
 #twitter.py
 
-#here's another comment
+import urllib
+import json
+import sys
+
+def search_twitter(query='mailchimp'):
+  url = 'http://search.twitter.com/search.json?q=' + query
+  response = urllib.urlopen(url).read()
+  data = json.loads(response)
+  return data['results']
+
+def print_tweets(tweets):
+  for tweet in tweets:
+    print tweet['from_user'] + ': ' + tweet['text'] + '\n'
+
+if __name__ == "__main__":
+  query = sys.argv[1]
+  results = search_twitter(query)
+  print_tweets(results)
+
+	
